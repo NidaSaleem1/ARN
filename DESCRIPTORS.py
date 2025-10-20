a79f91cc57392c1331aff0d22cebac9406cd2e12
nidasaleem027-oss <nidasaleem027@gmail.com>
2025-10-19 22:05:25 -0400

    Add files via upload

---
+++ DESCRIPTORS.py
@@ -0,0 +1,33 @@
+Chemical_Properties_of_Amines=[
+    "Amine 1 or Additive 1",
+    "1°, 2°, 3° Amine Ratio",
+    "MW or Mn (g/mol)",
+    "Organic Content on Adsorbent Basis (%)",
+    "N Content on Adsorbent Basis (mmol/g)",
+    "Amine 2 or Additive 2",
+    "Additive 3",
+    "Amine 2 or Additive 2  to Amine 1 Ratio",
+    "Additive 3 to Amine 1 Ratio",
+    "OH to N ratio",
+]
+
+Textural_Properties_of_Solid_Supports=[
+    "Support",
+    "BET Surface Area of Solid Support (m2g-1)",
+	"Pore Volume of Solid Support (cm3g-1)",
+    "Average Pore Diameters (nm)",
+
+]
+
+Operational_Parameters=[
+    "Relative Humidity (%)",
+    "CO2 Concentration (vol%)",
+    "Flow Rate (mL/min)",
+    "Adsorption Time (min)",
+    "Adsorption Temperature (℃)",
+    "Desorption Temperature (℃)",
+    "Cycle Number",
+]
+
+
+
